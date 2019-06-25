fdb3c33ac7b649e1591a790104ff4f58d0010690
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-25 17:16:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def find_the_cheese(strings)
   i = 0 
   cheese_types = ["cheddar", "gouda", "camembert"]
   while i < cheese_types.length 
-  return cheese_types[i] if array.include?cheese_types[i]
+  return cheese_types[i] if array.include?(cheese_types[i])
   i += 1 
-  
+  end
 end
